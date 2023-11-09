b234b1d3a1549cbef89dbd4d9b3b2862f7d7c251
huuthang106 <thangcachep106@gmail.com>
2023-11-09 21:03:24 +0700

    [HTML] init HTML Task 46 footer user

--- include/footer.php
+++ include/footer.php
@@ -1,76 +1,67 @@
 <footer class="footer-area section_gap">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-3  col-md-6 col-sm-6">
-					<div class="single-footer-widget">
-						<h6>About Us</h6>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore dolore
-							magna aliqua.
-						</p>
-					</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-3  col-md-6 col-sm-6">
+				<div class="single-footer-widget">
+					<h6>Về chúng tôi</h6>
+					<p>
+						Chúng tôi luôn muốn mang đến cho khách hàng một chảy nghiệm tốt nhất từ mặt hàng chúng tôi mang đến. Với một
+						đội ngũ chăm sóc khách hàng tận tình, và các sản phẩm đa dạng hứa hẹn sẽ mang đến một chảy nghiệm tuyệt vời cho quý
+						khách hàng
+					</p>
 				</div>
-				<div class="col-lg-4  col-md-6 col-sm-6">
-					<div class="single-footer-widget">
-						<h6>Newsletter</h6>
-						<p>Stay update with our latest</p>
-						<div class="" id="mc_embed_signup">
-							<form target="_blank" novalidate="true" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01"
-							 method="get" class="form-inline">
+			</div>
+			<div class="col-lg-4  col-md-6 col-sm-6">
+				<div class="single-footer-widget">
+					<h6>Giấy phép</h6>
+					<p>Chúng tôi cam kết các hàng hóa đến tay quý khách luôn có nguồn gốc xuất xứ rõ ràng</p>
+					<div class="" id="mc_embed_signup">
+						<form target="_blank" novalidate="true" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="form-inline">
 
-								<div class="d-flex flex-row">
-
-									<input class="form-control" name="EMAIL" placeholder="Enter Email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Email '"
-									 required="" type="email">
+							<div class="d-flex flex-row">
 
 
-									<button class="click-btn btn btn-default"><i class="fa fa-long-arrow-right" aria-hidden="true"></i></button>
-									<div style="position: absolute; left: -5000px;">
-										<input name="b_36c4fd991d266f23781ded980_aefe40901a" tabindex="-1" value="" type="text">
-									</div>
-
-									<!-- <div class="col-lg-4 col-md-4">
+								<!-- <div class="col-lg-4 col-md-4">
 												<button class="bb-btn btn"><span class="lnr lnr-arrow-right"></span></button>
 											</div>  -->
-								</div>
-								<div class="info"></div>
-							</form>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-3  col-md-6 col-sm-6">
-					<div class="single-footer-widget mail-chimp">
-						<h6 class="mb-20">Instragram Feed</h6>
-						<ul class="instafeed d-flex flex-wrap">
-							<li><img src="./content/img/i1.jpg" alt=""></li>
-							<li><img src="./content/img/i2.jpg" alt=""></li>
-							<li><img src="./content/img/i3.jpg" alt=""></li>
-							<li><img src="./content/img/i4.jpg" alt=""></li>
-							<li><img src="./content/img/i5.jpg" alt=""></li>
-							<li><img src="./content/img/i6.jpg" alt=""></li>
-							<li><img src="./content/img/i7.jpg" alt=""></li>
-							<li><img src="./content/img/i8.jpg" alt=""></li>
-						</ul>
-					</div>
-				</div>
-				<div class="col-lg-2 col-md-6 col-sm-6">
-					<div class="single-footer-widget">
-						<h6>Follow Us</h6>
-						<p>Let us be social</p>
-						<div class="footer-social d-flex align-items-center">
-							<a href="#"><i class="fa fa-facebook"></i></a>
-							<a href="#"><i class="fa fa-twitter"></i></a>
-							<a href="#"><i class="fa fa-dribbble"></i></a>
-							<a href="#"><i class="fa fa-behance"></i></a>
-						</div>
+							</div>
+							<div class="info"></div>
+						</form>
 					</div>
 				</div>
 			</div>
-			<div class="footer-bottom d-flex justify-content-center align-items-center flex-wrap">
-				<p class="footer-text m-0"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-</p>
+
+			<div class="col-lg-2 col-md-6 col-sm-6">
+				<div class="single-footer-widget">
+					<h6>Trang </h6>
+
+					<a href="index.php?act=category" class="caterories_footer">
+						<p>Sản phẩm</p>
+					</a>
+					<a href="index.php?act=blog" class="caterories_footer" ><p>Blog</p></a>			
+					<a href="index.php?act=tracking" class="caterories_footer" ><p>Theo dõi đơn hàng</p></a>		
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-6 col-sm-6">
+				<div class="single-footer-widget">
+					<h6>Theo dõi chúng tôi</h6>
+					<p>WD18304- Nhóm 9 làng lá</p>
+					<div class="footer-social d-flex align-items-center">
+						<a href="#"><i class="fa fa-facebook"></i></a>
+						<a href="#"><i class="fa fa-twitter"></i></a>
+						<a href="#"><i class="fa fa-dribbble"></i></a>
+						<a href="#"><i class="fa fa-behance"></i></a>
+					</div>
+				</div>
 			</div>
 		</div>
-	</footer>
+		<div class="footer-bottom d-flex justify-content-center align-items-center flex-wrap">
+			<p class="footer-text m-0"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+				Copyright &copy;<script>
+					document.write(new Date().getFullYear());
+				</script> Nhóm 9 <i class="fa fa-heart-o" aria-hidden="true"></i> làng lá <a href="https://colorlib.com" target="_blank"></a>
+				<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+			</p>
+		</div>
+	</div>
+</footer>
