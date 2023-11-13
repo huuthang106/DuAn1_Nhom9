ac3d87cfa50d38006fbcad49213fce2b8a11adea
huuthang106 <thangcachep106@gmail.com>
2023-11-13 22:33:18 +0700

    [PHP] init PHP Task 62

--- dao/products.php
+++ dao/products.php
@@ -62,5 +62,21 @@
         }
         
     }
-
+    public function seach_product($name,$page,$items_per_page){
+        $db = new connect();
+    
+        // Tính toán vị trí bắt đầu của kết quả dựa trên trang hiện tại và số lượng sản phẩm trên mỗi trang
+        $start = ($page - 1) * $items_per_page;
+        $name = '%'.$name . '%';
+        // Truy vấn SQL để lấy danh sách sản phẩm với phân trang
+        $select = "SELECT * FROM products Where name like ? ORDER BY product_id DESC LIMIT $start, $items_per_page";
+        $result = $db->pdo_query($select, $name);
+    
+        // Đếm tổng số sản phẩm
+        $count_query = "SELECT COUNT(*) as total FROM products";
+        $total_products = $db->pdo_query($count_query);
+    
+        return array('products' => $result, 'total_products' => $total_products);
+    
+    }
     }
--- site/keyword_search.php
+++ site/keyword_search.php
@@ -1,7 +1,8 @@
 <body>
-	<?php
-	include("./include/nav.php");
-	?>
+	
+<?php
+include("./include/nav.php");
+?>
 	<!-- End Header Area -->
 
 	<!-- Start Banner Area -->
@@ -13,7 +14,7 @@
 					<nav class="d-flex align-items-center">
 						<a href="index.html">Trang chủ<span class="lnr lnr-arrow-right"></span></a>
 						<a href="#">sản phẩm</a>
-
+						
 					</nav>
 				</div>
 			</div>
@@ -26,66 +27,24 @@
 				<div class="sidebar-categories">
 					<div class="head">Danh mục</div>
 					<ul class="main-categories">
+					
 
-
-
-
-
+						
+						
+						
 						<li class="main-nav-list"><a data-toggle="collapse" href="#officeProduct" aria-expanded="false" aria-controls="officeProduct"><span class="lnr lnr-arrow-right"></span>Áo<span class="number">(77)</span></a>
 							<ul class="collapse" id="officeProduct" data-toggle="collapse" aria-expanded="false" aria-controls="officeProduct">
 								<li class="main-nav-list child"><a href="#">Áo thun<span class="number">(13)</span></a></li>
 								<li class="main-nav-list child"><a href="#">Quần<span class="number">(09)</span></a></li>
 								<li class="main-nav-list child"><a href="#">Giày<span class="number">(17)</span></a></li>
-
+								
 							</ul>
 						</li>
-
-
+						
+						
 					</ul>
 				</div>
-				<!-- <div class="sidebar-filter mt-50">
-					<div class="top-filter-head">Product Filters</div>
-					<div class="common-filter">
-						<div class="head">Brands</div>
-						<form action="#">
-							<ul>
-								<li class="filter-list"><input class="pixel-radio" type="radio" id="apple" name="brand"><label for="apple">Apple<span>(29)</span></label></li>
-								<li class="filter-list"><input class="pixel-radio" type="radio" id="asus" name="brand"><label for="asus">Asus<span>(29)</span></label></li>
-								<li class="filter-list"><input class="pixel-radio" type="radio" id="gionee" name="brand"><label for="gionee">Gionee<span>(19)</span></label></li>
-								<li class="filter-list"><input class="pixel-radio" type="radio" id="micromax" name="brand"><label for="micromax">Micromax<span>(19)</span></label></li>
-								<li class="filter-list"><input class="pixel-radio" type="radio" id="samsung" name="brand"><label for="samsung">Samsung<span>(19)</span></label></li>
-							</ul>
-						</form>
-					</div>
-					<div class="common-filter">
-						<div class="head">Color</div>
-						<form action="#">
-							<ul>
-								<li class="filter-list"><input class="pixel-radio" type="radio" id="black" name="color"><label for="black">Black<span>(29)</span></label></li>
-								<li class="filter-list"><input class="pixel-radio" type="radio" id="balckleather" name="color"><label for="balckleather">Black
-										Leather<span>(29)</span></label></li>
-								<li class="filter-list"><input class="pixel-radio" type="radio" id="blackred" name="color"><label for="blackred">Black
-										with red<span>(19)</span></label></li>
-								<li class="filter-list"><input class="pixel-radio" type="radio" id="gold" name="color"><label for="gold">Gold<span>(19)</span></label></li>
-								<li class="filter-list"><input class="pixel-radio" type="radio" id="spacegrey" name="color"><label for="spacegrey">Spacegrey<span>(19)</span></label></li>
-							</ul>
-						</form>
-					</div>
-					<div class="common-filter">
-						<div class="head">Price</div>
-						<div class="price-range-area">
-							<div id="price-range"></div>
-							<div class="value-wrapper d-flex">
-								<div class="price">Price:</div>
-								<span>$</span>
-								<div id="lower-value"></div>
-								<div class="to">to</div>
-								<span>$</span>
-								<div id="upper-value"></div>
-							</div>
-						</div>
-					</div>
-				</div> -->
+				
 			</div>
 			<div class="col-xl-9 col-lg-8 col-md-7">
 				<!-- Start Filter Bar -->
@@ -94,13 +53,13 @@
 						<select>
 							<option value="1">Xu hướng</option>
 							<option value="1">Mới nhất</option>
-
+						
 						</select>
 					</div>
 					<div class="sorting mr-auto">
 						<select>
 							<option value="1">10</option>
-
+							
 						</select>
 					</div>
 					<div class="pagination">
@@ -117,75 +76,98 @@
 				<!-- Start Best Seller -->
 				<section class="lattest-product-area pb-40 category-list">
 					<div class="row">
+						
+						<?php
+							$items_per_page = 9;
 
+							// Lấy số trang hiện tại từ tham số truyền vào hoặc mặc định là trang 1
+							$page = isset($_GET['page']) ? intval($_GET['page']) : 1;
+$name = isset($_POST['key']) ? $_POST['key'] : '';
+$products = new products();
+$product_list = $products->seach_product($name, $page, $items_per_page);
 
+// Kiểm tra xem có dữ liệu trả về hay không
+if (!empty($product_list['products'])) {
+    foreach ($product_list['products'] as $product) {
+        $product_id = $product['product_id'];
+        $category_id = $product['category_id'];
+        $name = $product['name'];
+        $picture = $product['picture'];
+        $color = $product['color'];
+        $size = $product['size'];
+        $price = $product['price'];
+        $content = $product['content'];
+        $single_product = "index.php?act=single-product&product_id=" . $product_id;
+
+        echo '
+        <div class="col-lg-4 col-md-6">
+            <div class="single-product">
+                <img class="img-fluid" src="./content/img/product/' . $picture . '" alt="">
+                <div class="product-details">
+                    <h6>' . $name . '</h6>
+                    <div class="price">
+                        <h6>' . $price . '</h6>
+                    </div>
+                    <div class="prd-bottom">
+                        <a href="" class="social-info">
+                            <span class="ti-bag"></span>
+                            <p class="hover-text">Thêm vào giỏ hàng</p>
+                        </a>
+                        <a href="" class="social-info">
+                            <span class="lnr lnr-heart"></span>
+                            <p class="hover-text">Yêu thích</p>
+                        </a>
+                        <a href="' . $single_product . '" class="social-info">
+                            <span class="lnr lnr-move"></span>
+                            <p class="hover-text">Chi tiết</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        ';
+    }
+} else {
+    echo '<div class="col-lg-4 col-md-6">
+	<p>			Không tìm thấy sản phẩm.</p>
+	</div>';
+}
+
+							
+
+						?>
 						<!-- single product -->
-						<div class="col-lg-4 col-md-6">
-							<div class="single-product">
-								<img class="img-fluid" src="./content/img/product/p5.jpg" alt="">
-								<div class="product-details">
-									<h6>Giày</h6>
-									<div class="price">
-										<h6>$150.00</h6>
-										<h6 class="l-through">$210.00</h6>
-									</div>
-									<div class="prd-bottom">
-
-										<a href="" class="social-info">
-											<span class="ti-bag"></span>
-											<p class="hover-text">Thêm vào giỏ hàng</p>
-										</a>
-										<a href="" class="social-info">
-											<span class="lnr lnr-heart"></span>
-											<p class="hover-text">Yêu thích</p>
-										</a>
-
-										<a href="index.php?act=single-product" class="social-info">
-											<span class="lnr lnr-move"></span>
-											<p class="hover-text">Chi tiết</p>
-										</a>
-									</div>
-								</div>
-							</div>
-						</div>
-						<!-- single product -->
-						<div class="col-lg-4 col-md-6">
-							<div class="single-product">
-								<img class="img-fluid" src="./content/img/product/p6.jpg" alt="">
-								<div class="product-details">
-									<h6>Giày</h6>
-									<div class="price">
-										<h6>$150.00</h6>
-										<h6 class="l-through">$210.00</h6>
-									</div>
-									<div class="prd-bottom">
-
-										<a href="" class="social-info">
-											<span class="ti-bag"></span>
-											<p class="hover-text">Thêm vào giỏ hàng</p>
-										</a>
-										<a href="" class="social-info">
-											<span class="lnr lnr-heart"></span>
-											<p class="hover-text">Yêu thích</p>
-										</a>
-
-										<a href="index.php?act=single-product" class="social-info">
-											<span class="lnr lnr-move"></span>
-											<p class="hover-text">Chi tiết</p>
-										</a>
-									</div>
-								</div>
-							</div>
-						</div>
+						
+						
 					</div>
 				</section>
+				<?php
+					$total_pages = ceil($product_list['total_products'][0]['total'] / $items_per_page);
+
+					// Hiển thị phân trang
+					echo '<div class="filter-bar d-flex flex-wrap align-items-center">
+					<div class="sorting mr-auto">
+					<select>
+							<option value="1">10</option>
+							
+						</select>
+					</div>';
+					for ($i = 1; $i <= $total_pages; $i++) {
+						$active_class = ($i == $page) ? 'active' : '';
+						echo '<div class="pagination"' . $active_class . '">';
+						echo'<a href="index.php?act=category&page=' . $i . '">' . $i . '</a>';
+						
+						echo '</div>';
+					}
+					echo '</div>';
+				?>
 				<!-- End Best Seller -->
 				<!-- Start Filter Bar -->
-				<div class="filter-bar d-flex flex-wrap align-items-center">
+				<!-- <div class="filter-bar d-flex flex-wrap align-items-center">
 					<div class="sorting mr-auto">
-						<select>
+					<select>
 							<option value="1">10</option>
-
+							
 						</select>
 					</div>
 					<div class="pagination">
@@ -197,7 +179,7 @@
 						<a href="#">6</a>
 						<a href="#" class="next-arrow"><i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
 					</div>
-				</div>
+				</div> -->
 				<!-- End Filter Bar -->
 			</div>
 		</div>
@@ -253,7 +235,7 @@
 								</div>
 							</div>
 						</div>
-
+						
 					</div>
 				</div>
 				<div class="col-lg-3">
@@ -275,3 +257,7 @@
 	<!-- End footer Area -->
 
 	<!-- Modal Quick Product View -->
+	
+
+
+
