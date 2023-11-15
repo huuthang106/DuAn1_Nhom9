d7b78f2d0ffb0a0a7a642a10b91e5d2491667bbe
chimil1 <milccpc06135@fpt.edu.vn>
2023-11-15 20:20:46 +0700

    [PHP] init footer admin

---
+++ dao/footer.php
@@ -0,0 +1,21 @@
+      <!-- Footer -->
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+           <!-- <span>copyright &copy; <script> document.write(new Date().getFullYear()); </script> - developed by
+              <b><a href="#" target="_blank">indrijunanda</a></b>
+            </span>-->
+            <span>TRƯỜNG CAO ĐẲNG FPT POLYTECNHNIC </span>
+          </div>
+        </div>
+
+        <div class="container my-auto py-2">
+          <div class="copyright text-center my-auto">
+           <!-- <span>copyright &copy; <script> document.write(new Date().getFullYear()); </script> - distributed by
+              <b><a href="#" target="_blank">themewagon</a></b>
+            </span>-->
+            <span>Duan1-nhom9</span>
+          </div>
+        </div>
+      </footer>
+      <!-- Footer -->
