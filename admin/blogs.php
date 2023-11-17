2d9447490b3c9449bac09143edeeae7ecb904923
khuonghapc06329 <149867280+khuonghapc06329@users.noreply.github.com>
2023-11-17 19:52:55 +0700

    [PHP] init add,delete,update_blogs sửa lại bắt lỗi form categoies

--- admin/blogs.php
+++ admin/blogs.php
@@ -76,6 +76,44 @@
 
             </div>
 
+                <!--Row-->
+
+                <!-- Documentation Link -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <p>DataTables is a third party plugin that is used to generate the demo table below. For more
+                            information
+                            about DataTables, please visit the official <a href="https://datatables.net/"
+                                target="_blank">DataTables
+                                documentation.</a></p>
+                    </div>
+                </div>
+
+                <!-- Modal Logout -->
+                <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog"
+                    aria-labelledby="exampleModalLabelLogout" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabelLogout">Ohh No!</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <p>Are you sure you want to logout?</p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-outline-primary"
+                                    data-dismiss="modal">Cancel</button>
+                                <a href="login.html" class="btn btn-primary">Logout</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <!---Container Fluid-->
         </div>
 
         <!-- Footer -->
