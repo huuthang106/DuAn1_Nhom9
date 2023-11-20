f7630e6e9ecfc8b99fdc25d88f968e9e6834548b
huuthang106 <thangcachep106@gmail.com>
2023-11-20 21:01:27 +0700

    [HTML] sửa banner nhưng fall

--- site/index.php
+++ site/index.php
@@ -10,50 +10,7 @@
 	<section class="banner-area">
 		<div class="container">
 			<div class="row fullscreen align-items-center justify-content-start">
-				<div class="col-lg-12">
-					<div class="active-banner-slider owl-carousel">
-						<!-- single-slide -->
-						<div class="row single-slide align-items-center d-flex">
-							<div class="col-lg-5 col-md-6">
-								<div class="banner-content">
-									<h1>Giày mới <br>Nike</h1>
-									<p>Điều quan trọng là phải tự chăm sóc nỗi đau, sau đó là sự trưởng thành của bệnh nhân,
-										nhưng đồng thời cũng sẽ có rất nhiều công việc và nỗi đau. </p>
-									<div class="add-bag d-flex align-items-center">
-										<a class="add-btn" href="#"><span class="lnr lnr-cross"></span></a>
-										<span class="add-text text-uppercase">Thêm vào giỏ hàng</span>
-									</div>
-								</div>
-							</div>
-							<div class="col-lg-7">
-								<div class="banner-img">
-									<img class="img-fluid" src="./content/img/banner/banner-img.png" alt="">
-								</div>
-							</div>
-						</div>
-						<!-- single-slide -->
-						<div class="row single-slide align-items-center d-flex">
-							<div class="col-lg-5 col-md-6">
-								<div class="banner-content">
-									<h1>Giày mới <br>Nike</h1>
-									<p>Điều quan trọng là phải tự chăm sóc nỗi đau, sau đó là sự trưởng thành của bệnh nhân,
-										nhưng đồng thời cũng sẽ có rất nhiều công việc và nỗi đau. </p>
-									<div class="add-bag d-flex align-items-center">
-										<a class="add-btn" href="#"><span class="lnr lnr-cross"></span></a>
-										<span class="add-text text-uppercase">Thêm vào giỏ hàng</span>
-									</div>
-								</div>
-							</div>
-							<div class="col-lg-7">
-								<div class="banner-img">
-									<img class="img-fluid" src="./content/img/banner/banner-img.png" alt="">
-								</div>
-							</div>
-						</div>
-
-					</div>
-				</div>
-
+				
 			</div>
 		</div>
 	</section>
