73b2babe3fe4ed9835dac108680bb62b6c71be3e
chimil1 <milccpc06135@fpt.edu.vn>
2023-11-23 14:35:03 +0700

    [PHP - User] xử lý hiển thị trang chủ sản phẩm bạn có thể quan tâm

--- site/index.php
+++ site/index.php
@@ -17,122 +17,190 @@
 
     <!-- End banner Area -->
 
-    <!-- start features Area -->
-    <section class="features-area section_gap">
-        <div class="container">
-            <div class="row features-inner">
-                <!-- single features -->
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="single-features">
-                        <div class="f-icon">
-                            <img src="./content/img/features/f-icon1.png" alt="">
-                        </div>
-                        <h6>Giao hàng miễn phí</h6>
-                        <p>Miễn phí vận chuyển cho mọi đơn hàng</p>
-                    </div>
-                </div>
-                <!-- single features -->
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="single-features">
-                        <div class="f-icon">
-                            <img src="./content/img/features/f-icon2.png" alt="">
-                        </div>
-                        <h6>Chính sách hoàn trả</h6>
-                        <p>Miễn phí vận chuyển cho mọi đơn hàng</p>
-                    </div>
-                </div>
-                <!-- single features -->
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="single-features">
-                        <div class="f-icon">
-                            <img src="./content/img/features/f-icon3.png" alt="">
-                        </div>
-                        <h6>Hỗ trợ 24/7</h6>
-                        <p>Free Shipping on all order</p>
-                    </div>
-                </div>
-                <!-- single features -->
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="single-features">
-                        <div class="f-icon">
-                            <img src="./content/img/features/f-icon4.png" alt="">
-                        </div>
-                        <h6>Thanh toán an toàn</h6>
-                        <p>Miễn phí vận chuyển cho mọi đơn hàng</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- end features Area -->
+	<!-- start features Area -->
+	<section class="features-area section_gap">
+		<div class="container">
+			<div class="row features-inner">
+				<!-- single features -->
+				<div class="col-lg-3 col-md-6 col-sm-6">
+					<div class="single-features">
+						<div class="f-icon">
+							<img src="./content/img/features/f-icon1.png" alt="">
+						</div>
+						<h6>Giao hàng miễn phí</h6>
+						<p>Miễn phí vận chuyển cho mọi đơn hàng</p>
+					</div>
+				</div>
+				<!-- single features -->
+				<div class="col-lg-3 col-md-6 col-sm-6">
+					<div class="single-features">
+						<div class="f-icon">
+							<img src="./content/img/features/f-icon2.png" alt="">
+						</div>
+						<h6>Chính sách hoàn trả</h6>
+						<p>Miễn phí vận chuyển cho mọi đơn hàng</p>
+					</div>
+				</div>
+				<!-- single features -->
+				<div class="col-lg-3 col-md-6 col-sm-6">
+					<div class="single-features">
+						<div class="f-icon">
+							<img src="./content/img/features/f-icon3.png" alt="">
+						</div>
+						<h6>Hỗ trợ 24/7</h6>
+						<p>Free Shipping on all order</p>
+					</div>
+				</div>
+				<!-- single features -->
+				<div class="col-lg-3 col-md-6 col-sm-6">
+					<div class="single-features">
+						<div class="f-icon">
+							<img src="./content/img/features/f-icon4.png" alt="">
+						</div>
+						<h6>Thanh toán an toàn</h6>
+						<p>Miễn phí vận chuyển cho mọi đơn hàng</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- end features Area -->
+	<!-- Start related-product Area -->
+	<section class="related-product-area section_gap_bottom">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-lg-6 text-center">
+					<div class="section-title">
+						<h1>Có thể bạn quan tâm</h1>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-9">
+					<div class="row">
+						<?php
+						$product = new products();
 
-    <!-- Start category Area -->
-    <section class="category-area">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-lg-8 col-md-12">
-                    <div class="row">
-                        <div class="col-lg-8 col-md-8">
-                            <div class="single-deal">
-                                <div class="overlay"></div>
-                                <img class="img-fluid w-100" src="./content/img/category/c1.jpg" alt="">
-                                <a href="./content/img/category/c1.jpg" class="img-pop-up" target="_blank">
-                                    <div class="deal-details">
-                                        <h6 class="deal-title">Giày thể thao dành cho thể thao</h6>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-lg-4 col-md-4">
-                            <div class="single-deal">
-                                <div class="overlay"></div>
-                                <img class="img-fluid w-100" src="./content/img/category/c2.jpg" alt="">
-                                <a href="./content/img/category/c2.jpg" class="img-pop-up" target="_blank">
-                                    <div class="deal-details">
-                                        <h6 class="deal-title">Giày thể thao dành cho thể thao</h6>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-lg-4 col-md-4">
-                            <div class="single-deal">
-                                <div class="overlay"></div>
-                                <img class="img-fluid w-100" src="./content/img/category/c3.jpg" alt="">
-                                <a href="./content/img/category/c3.jpg" class="img-pop-up" target="_blank">
-                                    <div class="deal-details">
-                                        <h6 class="deal-title">Giày cho cặp đôi</h6>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-lg-8 col-md-8">
-                            <div class="single-deal">
-                                <div class="overlay"></div>
-                                <img class="img-fluid w-100" src="./content/img/category/c4.jpg" alt="">
-                                <a href="./content/img/category/c4.jpg" class="img-pop-up" target="_blank">
-                                    <div class="deal-details">
-                                        <h6 class="deal-title">Giày thể thao dành cho thể thao</h6>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <div class="single-deal">
-                        <div class="overlay"></div>
-                        <img class="img-fluid w-100" src="./content/img/category/c5.jpg" alt="">
-                        <a href="./content/img/category/c5.jpg" class="img-pop-up" target="_blank">
-                            <div class="deal-details">
-                                <h6 class="deal-title">Giày thể thao dành cho thể thao</h6>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- End category Area -->
+						foreach ($product->get_nine_product() as $key) {
+							extract($key);
+							$single_product = "index.php?act=single-product&product_id=".$product_id;
+							echo '
+							<div class="col-lg-4 col-md-6">
+							<div class="single-product">
+								<img class="img-fluid" src="./content/img/product/'.$picture.'" alt="">
+								<div class="product-details">
+									<h6>'.$name.'</h6>
+									<div class="price">
+										<h6>'.$price.'</h6>
+										
+									</div>
+									<div class="prd-bottom">
+
+										<a href="" class="social-info">
+											<span class="ti-bag"></span>
+											<p class="hover-text">Thêm vào giỏ hàng</p>
+										</a>
+										<a href="" class="social-info">
+											<span class="lnr lnr-heart"></span>
+											<p class="hover-text">Yêu thích</p>
+										</a>
+										
+										<a href="'.$single_product.'" class="social-info">
+											<span class="lnr lnr-move"></span>
+											<p class="hover-text">Chi tiết</p>
+										</a>
+									</div>
+								</div>
+							</div>
+						</div>
+								';
+						}
+						?>
+
+
+					</div>
+				</div>
+				<div class="col-lg-3">
+					<div class="ctg-right">
+						<a href="#" target="_blank">
+							<img class="img-fluid d-block mx-auto" src="./content/img/category/c7.png" alt="">
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- End related-product Area -->
+
+	<!-- start footer Area -->
+
+	<!-- Start category Area 
+	<section class="category-area">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-lg-8 col-md-12">
+					<div class="row">
+						<div class="col-lg-8 col-md-8">
+							<div class="single-deal">
+								<div class="overlay"></div>
+								<img class="img-fluid w-100" src="./content/img/category/c1.jpg" alt="">
+								<a href="./content/img/category/c1.jpg" class="img-pop-up" target="_blank">
+									<div class="deal-details">
+										<h6 class="deal-title">Giày thể thao dành cho thể thao</h6>
+									</div>
+								</a>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-4">
+							<div class="single-deal">
+								<div class="overlay"></div>
+								<img class="img-fluid w-100" src="./content/img/category/c2.jpg" alt="">
+								<a href="./content/img/category/c2.jpg" class="img-pop-up" target="_blank">
+									<div class="deal-details">
+										<h6 class="deal-title">Giày thể thao dành cho thể thao</h6>
+									</div>
+								</a>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-4">
+							<div class="single-deal">
+								<div class="overlay"></div>
+								<img class="img-fluid w-100" src="./content/img/category/c3.jpg" alt="">
+								<a href="./content/img/category/c3.jpg" class="img-pop-up" target="_blank">
+									<div class="deal-details">
+										<h6 class="deal-title">Giày cho cặp đôi</h6>
+									</div>
+								</a>
+							</div>
+						</div>
+						<div class="col-lg-8 col-md-8">
+							<div class="single-deal">
+								<div class="overlay"></div>
+								<img class="img-fluid w-100" src="./content/img/category/c4.jpg" alt="">
+								<a href="./content/img/category/c4.jpg" class="img-pop-up" target="_blank">
+									<div class="deal-details">
+										<h6 class="deal-title">Giày thể thao dành cho thể thao</h6>
+									</div>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6">
+					<div class="single-deal">
+						<div class="overlay"></div>
+						<img class="img-fluid w-100" src="./content/img/category/c5.jpg" alt="">
+						<a href="./content/img/category/c5.jpg" class="img-pop-up" target="_blank">
+							<div class="deal-details">
+								<h6 class="deal-title">Giày thể thao dành cho thể thao</h6>
+							</div>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>-->
+	</section>
+	<!-- End category Area -->
 
     <!-- start product Area -->
 
